fadbafea98f17a7f93c47d43da99bf1df67ad407
Irwin-S <irwin.r.singh@gmail.com>
2020-11-22 16:07:05 -0700

    Minor Tweak: Added drop down for Grade Levels

--- TerraformMinds/Controllers/AdministratorController.cs
+++ TerraformMinds/Controllers/AdministratorController.cs
@@ -10,6 +10,14 @@ using TerraformMinds.Models.Exceptions;
 
 namespace TerraformMinds.Controllers
 {
+    /* DOC STRING 
+     * @ Param =
+     * 
+     * @ summary = 
+     * 
+     * @ return = 
+     */
+
     public class AdministratorController : Controller
     {
         ValidationException exception = new ValidationException();
@@ -31,43 +39,43 @@ namespace TerraformMinds.Controllers
             return View();
         }
 
-        //public IActionResult Delete(string courseID)
+        //public void CreateCourse(string userID, string courseName, string subject, string courseDescription, string gradeLevel, DateTime startDate, DateTime endDate, int currentCapacity, int maxCapacity)
         //{
-        //    try
+        //    using (LearningManagementContext context = new LearningManagementContext())
         //    {
-        //        DeleteCourseByID(courseID);
+        //        context.Courses.Add(new Course()
+        //        {
+        //            UserID = int.Parse(userID),
+        //            CourseName = courseName,
+        //            Subject = subject,
+        //            CourseDescription = courseDescription,
+        //            GradeLevel = gradeLevel,
+        //            StartDate = startDate,
+        //            EndDate = endDate,
+        //            CurrentCapacity = currentCapacity,
+        //            MaxCapacity = maxCapacity
+        //        });
+        //        context.SaveChanges();
         //    }
-        //    catch (ValidationException e)
-        //    {
-        //        ViewBag.Courses = GetCourseByID(courseID);
-        //        ViewBag.Message = "Could not delete course, there exist problem(s) with your submission, see below.";
-        //        ViewBag.Exception = e;
-        //        ViewBag.Error = true;
-
-        //        return View("Details", new Dictionary<string, string>() { { "courseID", courseID } });
-        //    }
-
-        //    return RedirectToAction("List");
         //}
 
-        public void CreateCourse(string userID, string courseName, string subject, string courseDescription, string gradeLevel, DateTime startDate, DateTime endDate, int currentCapacity, int maxCapacity)
+        // POST: Author/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,UserID,CourseName,Subject,CourseDescription,GradeLevel,StartDate,EndDate,CurrentCapacity,MaxCapacity")] Course course)
         {
             using (LearningManagementContext context = new LearningManagementContext())
             {
-                context.Courses.Add(new Course()
+                if (ModelState.IsValid)
                 {
-                    UserID = int.Parse(userID),
-                    CourseName = courseName,
-                    Subject = subject,
-                    CourseDescription = courseDescription,
-                    GradeLevel = gradeLevel,
-                    StartDate = startDate,
-                    EndDate = endDate,
-                    CurrentCapacity = currentCapacity,
-                    MaxCapacity = maxCapacity
-                });
-                context.SaveChanges();
+                    context.Add(course);
+                    await context.SaveChangesAsync();
+                    return RedirectToAction(nameof(List));
+                }
             }
+            return View(course);
         }
 
         public List<Course> GetCourses()
@@ -99,16 +107,6 @@ namespace TerraformMinds.Controllers
 
             }
         }
-
-        //public void DeleteCourseByID(string courseID)
-        //{
-        //    using (LearningManagementContext context = new LearningManagementContext())
-        //    {
-        //        context.Remove(GetCourseByID(courseID));
-        //        context.SaveChanges();
-        //    }
-        //}
-
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -170,6 +168,13 @@ namespace TerraformMinds.Controllers
                 ViewBag.StartDate = course.StartDate;
                 ViewBag.EndDate = course.EndDate;
                 ViewBag.CourseDescription = course.CourseDescription;
+
+                // Create list if GradeLevels 
+                var gradeLevels = new List<string>() { "Kindergarten", "Grade 1", "Grade 2", "Grade 3", "Grade 4", "Grade 5", "Grade 6", "Grade 7", "Grade 8", "Grade 9", "Grade 10", "Grade 11", "Grade 12" };
+                // Populate dropdown list for grade levels.
+                var gradeLevelList = new SelectList(gradeLevels.ToDictionary(g => g, g => g), "Key", "Value", course.GradeLevel);
+                ViewBag.GradeLevels = gradeLevelList;
+
                 ViewBag.GradeLevel = course.GradeLevel;
                 ViewBag.CurrentCapacity = course.CurrentCapacity;
                 ViewBag.MaxCapacity = course.MaxCapacity;
@@ -198,7 +203,6 @@ namespace TerraformMinds.Controllers
             }
         }
 
-        // POST: Author/Delete
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
