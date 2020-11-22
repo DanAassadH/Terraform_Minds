df440921314cfd6d6654fb196d5b0fae31438763
Irwin-S <irwin.r.singh@gmail.com>
2020-11-22 16:27:06 -0700

    Added Create Course

--- TerraformMinds/Controllers/AdministratorController.cs
+++ TerraformMinds/Controllers/AdministratorController.cs
@@ -27,8 +27,31 @@ namespace TerraformMinds.Controllers
             return View();
         }
 
-        public IActionResult Create()
+        public IActionResult Create(string instructor, string courseName, string subject, string courseDescription, string gradeLevel, DateTime startDate, DateTime endDate, int currentCapacity, int maxCapacity)
         {
+            using(LearningManagementContext context = new LearningManagementContext())
+            {
+                var instructors = new SelectList(context.Users.Where(x => x.Role == 2)
+                .OrderBy(y => y.LastName)
+                .ToDictionary(us => us.ID, us => us.FirstName + " " + us.LastName), "Key", "Value", instructor);
+                ViewBag.Instructors = instructors;
+                ViewBag.CourseName = courseName;
+                ViewBag.Subject = subject;
+                ViewBag.StartDate = startDate;
+                ViewBag.EndDate = endDate;
+                ViewBag.CourseDescription = courseDescription;
+
+                // Create list if GradeLevels 
+                var gradeLevels = new List<string>() { "Kindergarten", "Grade 1", "Grade 2", "Grade 3", "Grade 4", "Grade 5", "Grade 6", "Grade 7", "Grade 8", "Grade 9", "Grade 10", "Grade 11", "Grade 12" };
+                // Populate dropdown list for grade levels.
+                var gradeLevelList = new SelectList(gradeLevels.ToDictionary(g => g, g => g), "Key", "Value", gradeLevel);
+                ViewBag.GradeLevels = gradeLevelList;
+
+                ViewBag.GradeLevel = gradeLevel;
+                ViewBag.CurrentCapacity = currentCapacity;
+                ViewBag.MaxCapacity = maxCapacity;
+            }
+
             return View();
         }
 
