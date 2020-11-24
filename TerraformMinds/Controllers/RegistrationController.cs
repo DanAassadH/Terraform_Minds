eb36166dbd6870f544340056039a3ab0bd14e5f3
Danish <danishriz100@gmail.comgit commitexit>
2020-11-23 21:54:54 -0700

    View Page created for Register new user

--- TerraformMinds/Controllers/RegistrationController.cs
+++ TerraformMinds/Controllers/RegistrationController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TerraformMinds.Models;
 
 namespace TerraformMinds.Controllers
 {
     public class RegistrationController : Controller
     {
+        /* -------------------------------------------- Actions -------------------------------------*/
+        public IActionResult Index()
+        {
+            return RedirectToAction("RegisterUser");
+           
+        }
+
         public IActionResult RegisterUser()
         {
+            /* If else to check if register new user button clicked*/
             return View();
         }
-    }
+
+
+        /* -------------------------------------------- Data -------------------------------------*/
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void Register(string firstName, string lastName , string email, string )
+        {
+            
+        }
+   }
 }
