8f285a3de2896d186bc56aa74b43e9668835773e
Danish <danishriz100@gmail.comgit commitexit>
2020-11-23 18:13:18 -0700

    custom Css button for register

---
+++ TerraformMinds/Controllers/RegistrationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TerraformMinds.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult RegisterUser()
+        {
+            return View();
+        }
+    }
+}
