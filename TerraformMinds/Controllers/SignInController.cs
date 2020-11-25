67ddabc69479bd6d684bc90036b84f937f1e98d0
Danish <danishriz100@gmail.comgit commitexit>
2020-11-25 12:11:48 -0700

    SetUp SignIn page

--- TerraformMinds/Controllers/SignInController.cs
+++ TerraformMinds/Controllers/SignInController.cs
@@ -8,7 +8,14 @@ namespace TerraformMinds.Controllers
 {
     public class SignInController : Controller
     {
+        /* ----------------------------------------------- Actions ----------------------------------------*/
         public IActionResult Index()
+        {
+            return RedirectToAction("SignIn");
+
+        }
+
+        public IActionResult SignIn()
         {
             return View();
         }
