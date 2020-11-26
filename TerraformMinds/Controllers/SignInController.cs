08dbfa67639df792dd8c5c0fdaab857ba5e8797a
Danish <danishriz100@gmail.comgit commitexit>
2020-11-26 11:13:22 -0700

    Sign In validation for success and not match

--- TerraformMinds/Controllers/SignInController.cs
+++ TerraformMinds/Controllers/SignInController.cs
@@ -3,6 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TerraformMinds.Models;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
+using TerraformMinds.Models.Exceptions;
 
 namespace TerraformMinds.Controllers
 {
@@ -15,9 +19,78 @@ namespace TerraformMinds.Controllers
 
         }
 
-        public IActionResult SignIn()
+        public IActionResult SignIn(string EMail, string Password)
         {
+            User user;
+            if (Request.Method == "POST")
+            {
+
+                try
+                {
+                    user = ValidateSignIn(EMail, Password);
+                    if (user != null)
+                    {
+                        ViewBag.Message = $"Yaya Successfully Logged In User!";
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Email and Password dont match";
+                        ViewBag.Error = true;
+                    }
+
+                }
+                catch (ValidationException e)
+                {
+                    ViewBag.Message = "Something went wrong";
+                    // ViewBag.Exception = e;
+                    ViewBag.Error = true;
+                }
+
+
+            }
+
             return View();
         }
+
+
+        [ValidateAntiForgeryToken]
+        public User ValidateSignIn(string EMail, string Password)
+        {
+            string password;
+            User validUser;
+
+            // Add Password Hash here
+            password = HashAndSaltPassowrd(Password, EMail);
+
+            using (LearningManagementContext context = new LearningManagementContext())
+            {
+                // Validating User
+                validUser = context.Users.Where(x => x.EMail.Equals(EMail) && x.Password.Equals(password)).SingleOrDefault();
+            }
+
+            return validUser;
+        }
+
+        public string HashAndSaltPassowrd(string password, string email)
+        {
+
+            // https://docs.microsoft.com/en-us/aspnet/core/security/data-protection/consumer-apis/password-hashing?view=aspnetcore-5.0
+            // Generate a SALT
+            // Convert string to a byte array Using a known salt insted of random . for proper security there should be a SALT field in database, and every password should have a different SALT(generated through random function) , but for this application we are using a combination of email+password to generate SALT 
+
+            byte[] salt = Encoding.ASCII.GetBytes(email + password);
+
+            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+
+            return hashed;
+        }
+
     }
 }
