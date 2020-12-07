9ac9c9ed6f1846d6014568a424bbf5ab31278a6e
Irwin-S <irwin.r.singh@gmail.com>
2020-12-07 02:16:51 -0700

    Added Validation to Admin Dashboard. Validation include: Edit Course Start and End Date prior to today date; Added Show All Filter to Course Page in Course Controller; Added Filter Function to Admin Course List

--- TerraformMinds/Controllers/AdministratorController.cs
+++ TerraformMinds/Controllers/AdministratorController.cs
@@ -141,14 +141,22 @@ namespace TerraformMinds.Controllers
                                 {
                                     if (course.StartDate < DateTime.Today)
                                     {
-                                        exception.ValidationExceptions.Add(new Exception("Invalid State Date: Course Start Date cannot be set in the past."));
+                                        exception.ValidationExceptions.Add(new Exception("Invalid Start Date: Course Start Date cannot be set prior to todays date."));
                                     }
 
                                     else
                                     {
-                                        if (context.Courses.Any(x => x.CourseName == course.CourseName))
+                                        if(course.EndDate < DateTime.Today)
                                         {
-                                            exception.ValidationExceptions.Add(new Exception("Invalid Course Name: That course already exists."));
+                                            exception.ValidationExceptions.Add(new Exception("Invalid End Date: Course End Date cannot be set prior to todays date."));
+                                        }
+
+                                        else
+                                        {
+                                            if (context.Courses.Any(x => x.CourseName == course.CourseName))
+                                            {
+                                                exception.ValidationExceptions.Add(new Exception("Invalid Course Name: That course already exists."));
+                                            }
                                         }
                                     }
                                 }
@@ -205,11 +213,40 @@ namespace TerraformMinds.Controllers
         /// Populates a list of courses in View/CourseList 
         /// </summary>
         /// <returns>Showcases a list of courses in View.CourseList</returns>
-        public IActionResult CourseList()
+        public IActionResult CourseList(string subjectFilter, string gradeFilter)
         {
-            ViewBag.Courses = GetCourses();
+            var gradeLevels = new List<string>() { "Kindergarten", "Grade 1", "Grade 2", "Grade 3", "Grade 4", "Grade 5", "Grade 6", "Grade 7", "Grade 8", "Grade 9", "Grade 10", "Grade 11", "Grade 12" };
+            var gradeLevelList = new SelectList(gradeLevels.ToDictionary(g => g, g => g), "Key", "Value", gradeLevels);
+            ViewBag.GradeLevels = gradeLevelList;
 
-            return View();
+            var courseSubjects = new List<string>() { "English", "Math", "Science", "Social Studies" };
+            var courseSubjectsList = new SelectList(courseSubjects.ToDictionary(s => s, s => s), "Key", "Value", courseSubjects);
+            ViewBag.CourseSubjects = courseSubjectsList;
+
+            if (gradeLevels.Contains(gradeFilter) && !courseSubjects.Contains(subjectFilter))
+            {
+                ViewBag.Courses = GetCoursesByGrade(gradeFilter);
+                ViewBag.Filter = true;
+            }
+
+            else if (courseSubjects.Contains(subjectFilter) && !gradeLevels.Contains(gradeFilter))
+            {
+                ViewBag.Courses = GetCoursesBySubject(subjectFilter);
+                ViewBag.Filter = true;
+            }
+
+            else if (courseSubjects.Contains(subjectFilter) && gradeLevels.Contains(gradeFilter))
+            {
+                ViewBag.Courses = GetCoursesByGradeAndSubject(subjectFilter, gradeFilter);
+                ViewBag.Filter = true;
+            }
+
+            else
+            {
+                ViewBag.Courses = GetCoursesAll();
+                ViewBag.Filter = false;
+            }
+            return View("CourseList");
         }
 
         /// <summary>
@@ -217,7 +254,7 @@ namespace TerraformMinds.Controllers
         /// Used Include to join User table
         /// </summary>
         /// <returns> A list of Courses based on data from the database</returns>
-        public List<Course> GetCourses()
+        public List<Course> GetCoursesAll()
         {
             List<Course> courseList;
             using (LearningManagementContext context = new LearningManagementContext())
@@ -225,6 +262,36 @@ namespace TerraformMinds.Controllers
                 courseList = context.Courses.Include(x => x.User).ToList();
             }
             return courseList;
+        }
+
+        public List<Course> GetCoursesByGrade(string gradeFilter)
+        {
+            List<Course> courseListByGrade;
+            using (LearningManagementContext context = new LearningManagementContext())
+            {
+                courseListByGrade = context.Courses.Where(x => x.GradeLevel == gradeFilter).Include(x => x.User).ToList();
+            }
+            return courseListByGrade;
+        }
+
+        public List<Course> GetCoursesBySubject(string subjectFilter)
+        {
+            List<Course> courseListBySubject;
+            using (LearningManagementContext context = new LearningManagementContext())
+            {
+                courseListBySubject = context.Courses.Where(x => x.Subject == subjectFilter).Include(x => x.User).ToList();
+            }
+            return courseListBySubject;
+        }
+
+        public List<Course> GetCoursesByGradeAndSubject(string subjectFilter, string gradeFilter)
+        {
+            List<Course> courseListBySubject;
+            using (LearningManagementContext context = new LearningManagementContext())
+            {
+                courseListBySubject = context.Courses.Where(x => x.Subject == subjectFilter && x.GradeLevel == gradeFilter).Include(x => x.User).ToList();
+            }
+            return courseListBySubject;
         }
 
         /// <summary>
@@ -344,6 +411,21 @@ namespace TerraformMinds.Controllers
                                 {
                                     exception.ValidationExceptions.Add(new Exception("Invalid Start Date: Course Start Date cannot be after End Date."));
                                 }
+                                else
+                                {
+                                    if (course.StartDate < DateTime.Today)
+                                    {
+                                        exception.ValidationExceptions.Add(new Exception("Invalid Start Date: Course Start Date cannot be set prior to todays date."));
+                                    }
+
+                                    else
+                                    {
+                                        if (course.EndDate < DateTime.Today)
+                                        {
+                                            exception.ValidationExceptions.Add(new Exception("Invalid End Date: Course End Date cannot be set prior to todays date."));
+                                        }
+                                    }
+                                }
                             }
                         }
 
@@ -398,7 +480,7 @@ namespace TerraformMinds.Controllers
                     ViewBag.MaxCapacity = course.MaxCapacity;
                 }   
             }
-            return View(course);
+            return View();
         }
 
         /// <summary>
